3bb850d961bf6c3f757035ac776d0742a06c0247
VictorVG <victorvg04@gmail.com>
2020-05-03 15:02:29 +0300

    Wrong BOM in file body

---
+++ test.lua
@@ -0,0 +1,14 @@
+﻿
+Macro{
+  id="72B3EE34-BFE1-43E0-BCA2-46D83E8CDF60";
+  area="Common";
+--  key="";
+--  description="";
+--  filemask="";
+--  flags="";
+--  priority=50;
+--  sortpriority=50;
+--  condition=function(key) return true end;
+  action=﻿function() base64.encode(Editor.SelValue)
+far.CopyToClipboard(str) end;
+}
